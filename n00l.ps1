02496340a116e3088ead28013e50caf8e9b17b9a
n00l <143655476+softpiano@users.noreply.github.com>
2025-04-16 22:35:17 +0300

    Update n00l.ps1

--- n00l.ps1
+++ n00l.ps1
@@ -5,5 +5,5 @@ Write-Host "
 | |\  || |_| | |_| || |___ 
 |_| \_| \___/ \___/ |_____|
                           
-        n00l              
+      - n00l -  Let's Rock!
 " -ForegroundColor Yellow -BackgroundColor Red
