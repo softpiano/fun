d30be29a6c04f592cc6ecbb70a9bb0ce962be152
n00l <143655476+softpiano@users.noreply.github.com>
2025-04-16 22:22:20 +0300

    Create n00l.ps1

---
+++ n00l.ps1
@@ -0,0 +1,9 @@
+Write-Host "
+ _   _   ___   ___   _     
+| \ | | / _ \ / _ \ | |    
+|  \| || | | | | | || |    
+| |\  || |_| | |_| || |___ 
+|_| \_| \___/ \___/ |_____|
+                          
+        n00l              
+" -ForegroundColor Yellow -BackgroundColor Red
